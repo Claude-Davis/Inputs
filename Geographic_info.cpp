27b50b29bab8f6c1479d9cd74d361c540edfa58f
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2023-12-20 13:52:13 -0500

    Create Geographic_info.cpp

---
+++ Geographic_info.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string country, country2, province;
+  cin >> country;
+  cout << "Country of Birth: " << country << endl;
+  cin >> country2;
+  cout << "Country of Residence: " << country2 << endl;
+  cin >> province;
+  cout << "Province/State of Residence: " << province << endl;
+}
