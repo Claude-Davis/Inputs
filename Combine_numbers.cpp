cb5c5c10f84b6807778be0e1239575f2662ff364
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2023-12-20 14:06:34 -0500

    Create Combine_numbers.cpp

---
+++ Combine_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+  cout << a + e << endl;
+  cout << d + b << endl;
+  cout << c << a << endl;
+  cout << e << "+" << d << "=" << e + d;
+}
