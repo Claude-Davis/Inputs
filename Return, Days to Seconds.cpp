3e34af508c0b983a2068ec20cdeb70714b40d746
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2023-12-31 18:47:58 -0500

    Create Return, Days to Seconds.cpp

---
+++ Return, Days to Seconds.cpp
@@ -0,0 +1,14 @@
+#including <iostream>
+using namespace std
+
+int toSeconds(int days) {
+    return(days*24*60*60);
+}
+
+int main() {
+    int days;
+    cin >> days;
+
+    int seconds = toSeconds(days);
+    cout << seconds;
+}
