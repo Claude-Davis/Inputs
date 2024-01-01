3d776a9d60f25dca0cdc3e79af884606af432169
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2023-12-31 19:38:59 -0500

    Create Boolean_Return.cpp

---
+++ Boolean_Return.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int subscription(int age) {
+  if(age>16) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+int main() {
+  int age;
+  cin >> age;
+}
