18de92256c9dcf821a3552124f7d46970e4ab5cd
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2024-01-06 19:18:21 -0500

    Update Return, Days to Seconds.cpp
    Updated to allow for inputs and outputs of type double.

--- Return, Days to Seconds.cpp
+++ Return, Days to Seconds.cpp
@@ -5,10 +5,20 @@ int toSeconds(int days) {
     return(days*24*60*60);
 }
 
+double toSeconds(double days) {
+    return(days*24*60*60);
+}
+
 int main() {
     int days;
     cin >> days;
 
     int seconds = toSeconds(days);
     cout << seconds;
+
+    double days2;
+    cin >> days2;
+
+    double seconds2 = toSeconds(days2);
+    cout << seconds2;
 }
